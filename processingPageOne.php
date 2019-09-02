6184a4cf68b0d75a0fb0fcf52822cf4cb28d6fb9
mavila7231 <53978303+mavila7231@users.noreply.github.com>
2019-09-02 13:03:20 -0400

    added codes to collect data from index.html page

--- processingPageOne.php
+++ processingPageOne.php
@@ -1,1 +1,23 @@
+<?php
 
+$name = $_POST['name'];
+$visitor_email = $POST['email'];
+$message = $_POST['messge'];
+
+$email_from = 'msavilaclass@gmail.com';
+$email_subject = "New Form Submission";
+$email_body = "User Name: $name.\n".
+  "User Email: $visitor_email.\n".
+  "User Message: $message.\n";
+
+$to = "msavilaclass@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: index.html");
+
+?>
