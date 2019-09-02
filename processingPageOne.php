cf12f9739860cc089543e568f3ab1f4c9b7da8d6
mavila7231 <53978303+mavila7231@users.noreply.github.com>
2019-09-02 13:52:31 -0400

    Update processingPageOne.php

--- processingPageOne.php
+++ processingPageOne.php
@@ -1,20 +1,20 @@
 <?php
 
 $name = $_POST['name'];
-$visitor_email = $POST['email'];
-$message = $_POST['messge'];
+$visitor_email =$POST['email'];
+$message =$_POST['messge'];
 
-$email_from = 'msavilaclass@gmail.com';
-$email_subject = "New Form Submission";
-$email_body = "User Name: $name.\n".
+$email_from ='msavilaclass@gmail.com';
+$email_subject ="New Form Submission";
+$email_body ="User Name: $name.\n".
   "User Email: $visitor_email.\n".
   "User Message: $message.\n";
 
-$to = "msavilaclass@gmail.com";
+$to ="msavilaclass@gmail.com";
 
-$headers = "From: $email_from \r\n";
+$headers ="From: $email_from \r\n";
 
-$headers .= "Reply-To: $visitor_email \r\n";
+$headers .="Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
